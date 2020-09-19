cbe51ad63f5a3fa90325ce54c65734074c28b010
Damian Wojsław <dwojslaw@redhat.com>
2020-09-19 19:41:13 +0200

    Start the source

---
+++ src/pingme.cpp
@@ -0,0 +1,5 @@
+//
+// Created by trochej on 9/19/20.
+//
+
+#include "pingme.h"
---
+++ src/pingme.h
@@ -0,0 +1,8 @@
+//
+// Created by trochej on 9/19/20.
+//
+
+#ifndef PINGME_PINGME_H
+#define PINGME_PINGME_H
+
+#endif //PINGME_PINGME_H
