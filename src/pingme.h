2d27423db20d4ac21f63d99e554392b6d508a7a0
Damian Wojsław <dwojslaw@redhat.com>
2020-10-15 14:51:46 +0200

    Add Qt support

--- src/pingme.h
+++ src/pingme.h
@@ -5,4 +5,20 @@
 #ifndef PINGME_PINGME_H
 #define PINGME_PINGME_H
 
+#include <qt5/QtWidgets/QMainWindow>
+#include <memory>
+
+namespace Ui {
+    class MainWindow;
+}
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+private:
+    std::shared_ptr<MainWindow> ui { std::make_shared<MainWindow>() };
+};
+
 #endif //PINGME_PINGME_H
